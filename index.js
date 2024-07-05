22fbd690bbe72e7e39c1a541accbebbb246acf0f
thegr8ivan <ivan.kamotho@student.moringaschoool.com>
2024-07-05 15:35:28 +0300

    adds index.js

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance) {
+    if (distance <=400) {
+        return 'This one is on me!';
+    }  else if (distance > 400 & distance <=2000) {
+        return  "That will be twenty bucks.";
+    }  else if (distance >2000 & distance <=2500) { 
+        return "I will gladly take your thirty bucks."; 
+    }  else {
+        return "No can do.";    
+    }
+}            
+function ternaryCheckCity(city) {
+    return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+}    
+function switchOnCharmFromTip(tip) {
+    let response;
+    switch (tip) {
+        case "generous":
+            response = "Thank you so much.";
+            break;
+        case "not as generous":
+            response = "Thank you.";
+            break;
+        case "Thank you everything":
+            response = "Bye.";
+            break;
+        default:
+            response = "Bye.";            
+    }
+    return response;
 }
-
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+    
